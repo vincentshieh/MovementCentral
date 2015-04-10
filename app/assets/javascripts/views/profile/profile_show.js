dad5068fce59d33c86dc5de88af85e91ff2c60ea
Vincent Shieh <vincent.shieh@gmail.com>
2015-04-09 17:50:27 -0700

    get Add Friend button working

--- app/assets/javascripts/views/profile/profile_show.js
+++ app/assets/javascripts/views/profile/profile_show.js
@@ -1,13 +1,17 @@
 MovementCentral.Views.ProfileShow = Backbone.CompositeView.extend({
   template: JST['profile/show'],
 
+  events: {
+    'click .friend-button': 'handleFriendClick'
+  },
+
   initialize: function (options) {
     this.user_id = options.user_id;
     this.friendships = options.friendships;
-    this.listenTo(this.friendships, 'sync', this.render);
+    this.listenTo(this.friendships, 'sync add', this.render);
   },
 
-  friendButton: function () {
+  friendButtonVal: function () {
     var user_id = this.user_id;
 
     for(var i = 0; i < this.friendships.length; i++) {
@@ -28,10 +32,31 @@ MovementCentral.Views.ProfileShow = Backbone.CompositeView.extend({
     }
   },
 
+  handleFriendClick: function (event) {
+    var friendButtonVal = $(event.currentTarget).text(),
+      view = this;
+
+    if (friendButtonVal === "Add Friend") {
+      var friendship = new MovementCentral.Models.Friendship({
+        requester_id: MovementCentral.current_user_id,
+        requestee_id: this.user_id
+      });
+      friendship.save({}, {
+        success: function () {
+          view.friendships.add(new MovementCentral.Models.Friendship({
+            user_id: view.user_id,
+            accepted: false
+          }));
+          Backbone.history.navigate("#users/" + view.user_id, { trigger: true });
+        }
+      });
+    }
+  },
+
   render: function () {
     var renderedContent = this.template({
       user_id: this.user_id,
-      friend_button: this.friendButton()
+      friend_button_val: this.friendButtonVal()
     });
     this.$el.html(renderedContent);
     this.renderPostForm();
