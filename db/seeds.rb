bc39e103291fd5e3b9b430d049561f5217764327
Vincent Shieh <vincent.shieh@gmail.com>
2015-04-12 16:24:27 -0700

    add seed data for posts, comments, friendships, and likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,3 +48,85 @@ User.create!(email: 'jerb@aa.com', password: 'jeroboam',
              fname: 'Jeroboam', lname: 'Bozeman', dance_style: 'Modern',
              gender: 'Male', company: 'Alvin Ailey American Dance Theater',
              profile_picture: 'http://res.cloudinary.com/sagacity/image/upload/c_crop,h_736,w_640,x_0,y_34/c_fit,w_640/Jeroboam_Bozeman_Photo_Eduardo_Patino_izpuof.jpg')
+
+Friendship.create!(requester_id: 1, requestee_id: 2, accepted: true)
+Friendship.create!(requester_id: 1, requestee_id: 3, accepted: true)
+Friendship.create!(requester_id: 1, requestee_id: 4, accepted: true)
+Friendship.create!(requester_id: 5, requestee_id: 1, accepted: false)
+Friendship.create!(requester_id: 6, requestee_id: 1, accepted: true)
+Friendship.create!(requester_id: 7, requestee_id: 1, accepted: true)
+Friendship.create!(requester_id: 8, requestee_id: 1, accepted: true)
+Friendship.create!(requester_id: 1, requestee_id: 9, accepted: true)
+Friendship.create!(requester_id: 1, requestee_id: 10, accepted: true)
+Friendship.create!(requester_id: 11, requestee_id: 1, accepted: false)
+Friendship.create!(requester_id: 2, requestee_id: 3, accepted: true)
+Friendship.create!(requester_id: 2, requestee_id: 4, accepted: true)
+Friendship.create!(requester_id: 3, requestee_id: 4, accepted: true)
+Friendship.create!(requester_id: 3, requestee_id: 7, accepted: false)
+Friendship.create!(requester_id: 3, requestee_id: 9, accepted: true)
+Friendship.create!(requester_id: 4, requestee_id: 6, accepted: true)
+Friendship.create!(requester_id: 5, requestee_id: 8, accepted: false)
+Friendship.create!(requester_id: 5, requestee_id: 9, accepted: true)
+Friendship.create!(requester_id: 5, requestee_id: 11, accepted: false)
+Friendship.create!(requester_id: 9, requestee_id: 6, accepted: true)
+Friendship.create!(requester_id: 6, requestee_id: 7, accepted: true)
+Friendship.create!(requester_id: 7, requestee_id: 8, accepted: true)
+Friendship.create!(requester_id: 7, requestee_id: 9, accepted: true)
+Friendship.create!(requester_id: 8, requestee_id: 9, accepted: false)
+Friendship.create!(requester_id: 9, requestee_id: 10, accepted: true)
+Friendship.create!(requester_id: 9, requestee_id: 11, accepted: true)
+Friendship.create!(requester_id: 10, requestee_id: 11, accepted: true)
+
+Post.create!(author_id: 1, recipient_id: 1,
+             content: 'Always a blast performing at Queensland Performing Arts Center. Thanks for the support, Australia!',
+             picture: 'http://www.trbimg.com/img-542b28a0/turbine/ct-misty-copeland-american-ballet-theatre')
+Post.create!(author_id: 2, recipient_id: 1,
+             content: 'my calves are so sore from taking your class yesterday')
+Post.create!(author_id: 6, recipient_id: 1,
+             content: 'Great having you as a guest judge last week. Good luck on tour!')
+Post.create!(author_id: 4, recipient_id: 1,
+             content: 'You were amazing in NYC :)')
+Post.create!(author_id: 1, recipient_id: 1,
+             content: 'Check out Under Armour!',
+             picture: 'https://s-media-cache-ak0.pinimg.com/736x/c2/4e/dd/c24eddada7b47ce854946ed508858c1b.jpg')
+Post.create!(author_id: 3, recipient_id: 4,
+             content: 'Victorious at Blackpool 2014!',
+             picture: 'https://s-media-cache-ak0.pinimg.com/236x/23/6e/d7/236ed703a2ebc9ab862a29619f112dd9.jpg')
+Post.create!(author_id: 7, recipient_id: 7,
+             content: '#fam #whatdoyouwantmos?',
+             picture: 'http://www.mtv.com/onair/dance_crew/season_7/photos/cast/mos_wanted_crew/flipbook/group.jpg')
+Post.create!(author_id: 10, recipient_id: 11,
+             content: 'carpool to rehearsal tmrw?')
+
+Comment.create!(author_id: 1, post_id: 2,
+                content: 'just means you gotta train harder ;)')
+Comment.create!(author_id: 1, post_id: 3,
+                content: 'It was my pleasure!')
+Comment.create!(author_id: 7, post_id: 6,
+                content: 'Nicely done')
+Comment.create!(author_id: 2, post_id: 6,
+                content: 'No surprise there')
+Comment.create!(author_id: 1, post_id: 6,
+                content: 'Congrats guys!')
+Comment.create!(author_id: 11, post_id: 8,
+                content: 'alright, just remember to set your alarm this time')
+
+Like.create!(user_id: 1, likable_type: 'Post', likable_id: 6)
+Like.create!(user_id: 2, likable_type: 'Post', likable_id: 6)
+Like.create!(user_id: 6, likable_type: 'Post', likable_id: 6)
+Like.create!(user_id: 7, likable_type: 'Post', likable_id: 6)
+Like.create!(user_id: 9, likable_type: 'Post', likable_id: 6)
+Like.create!(user_id: 1, likable_type: 'Post', likable_id: 7)
+Like.create!(user_id: 6, likable_type: 'Post', likable_id: 7)
+Like.create!(user_id: 8, likable_type: 'Post', likable_id: 7)
+Like.create!(user_id: 9, likable_type: 'Post', likable_id: 7)
+Like.create!(user_id: 2, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 3, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 4, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 6, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 7, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 8, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 9, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 10, likable_type: 'Post', likable_id: 1)
+Like.create!(user_id: 3, likable_type: 'Comment', likable_id: 1)
+Like.create!(user_id: 4, likable_type: 'Comment', likable_id: 1)
