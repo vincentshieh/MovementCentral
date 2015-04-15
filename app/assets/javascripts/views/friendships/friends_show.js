b8b9831d15c0b10b8b9231e5fd78285e87713b1d
Vincent Shieh <vincent.shieh@gmail.com>
2015-04-15 16:51:37 -0700

    style friend items and implement navigation to friend's profile page

--- app/assets/javascripts/views/friendships/friends_show.js
+++ app/assets/javascripts/views/friendships/friends_show.js
@@ -1,6 +1,10 @@
 MovementCentral.Views.FriendsShow = Backbone.View.extend({
   template: JST['friendships/friends_show'],
 
+  events: {
+    'click .friend-item': 'handleFriendItemClick'
+  },
+
   initialize: function (options) {
     this.user_id = options.user_id;
     this.collection.fetch({
@@ -10,6 +14,12 @@ MovementCentral.Views.FriendsShow = Backbone.View.extend({
       }
     });
     this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  handleFriendItemClick: function (event) {
+    $target = $(event.currentTarget);
+    var user_id = $target.data('user-id');
+    Backbone.history.navigate("#/users/" + user_id, { trigger: true });
   },
 
   render: function () {
