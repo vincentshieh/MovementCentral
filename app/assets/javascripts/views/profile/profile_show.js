1f64535e3bf20a5831e52375941e651e92a18629
Vincent Shieh <vincent.shieh@gmail.com>
2015-04-17 12:05:00 -0700

    implement change cover photo

--- app/assets/javascripts/views/profile/profile_show.js
+++ app/assets/javascripts/views/profile/profile_show.js
@@ -4,7 +4,8 @@ MovementCentral.Views.ProfileShow = Backbone.CompositeView.extend({
   events: {
     'click .friend-button': 'handleFriendClick',
     'click .subview-btn': 'handleSubviewClick',
-    'click .change-profile-pic': 'changeProfilePic'
+    'click .change-profile-pic': 'changeProfilePic',
+    'click .change-cover-photo': 'changeCoverPhoto'
   },
 
   initialize: function (options) {
@@ -21,6 +22,27 @@ MovementCentral.Views.ProfileShow = Backbone.CompositeView.extend({
       this.listenTo(this.posts, 'sync', this.render);
     }
     this.listenTo(this.friendships, 'sync', this.render);
+  },
+
+  changeCoverPhoto: function (event) {
+    event.preventDefault();
+    var current_user = new MovementCentral.Models.Friendship();
+    var view = this;
+
+    filepicker.pick(
+      {
+        mimetypes: ['image/*'],
+        services: ['COMPUTER']
+      },
+      function (Blob) {
+        current_user.save({ cover_photo: Blob.url }, {
+          url: 'api/users_photo',
+          success: function () {
+            view.friendships.fetch();
+          }
+        });
+      }
+    );
   },
 
   changeProfilePic: function (event) {
